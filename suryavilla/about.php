500acbdd15b4d4bee49a7f1e8f00028d4c06645e
Vaishnavi Baporikar <vaishnavibaporikar10@gmail.com>
2021-02-10 17:05:20 +0530

    html file to php file
    10/2/21

--- suryavilla/about.php
+++ suryavilla/about.php
@@ -17,64 +17,7 @@
     </div><!-- End Preload -->
     
     <!-- Header ================================================== -->
-    <header>
-    <div class="container">
-        <div class="row">
-            <div class="col--md-3 col-sm-3 col-xs-3">
-                <a href="index.html" id="logo">
-                <img src="../img/logo.png" width="190" height="23" alt="" data-retina="true">
-                </a>
-            </div>
-            <nav class="col--md-9 col-sm-9 col-xs-9">
-            <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="javascript:void(0);"><span>Menu mobile</span></a>
-            <ul id="lang_top">
-                <li><a href="#" class="active">EN</a></li>
-                <li><a href="#">FR</a></li>
-                <li><a href="#">ES</a></li>
-            </ul>
-            <div class="main-menu">
-                <div id="header_menu">
-                     <img src="../img/logo_m.png" width="141" height="40" alt="" data-retina="true">
-                </div>
-                <a href="#" class="open_close" id="close_in"><i class="icon_set_1_icon-77"></i></a>
-                 <ul>
-                    <li class="submenu">
-                    <a href="javascript:void(0);" class="show-submenu">Home<i class="icon-down-open-mini"></i></a>
-                    <ul>
-                        <li><a href="index.html">Home Booking</a></li>
-                        <li><a href="index_5.html">Home Booking date 2</a></li>
-                        <li><a href="index_4.html">Home Carousel</a></li>
-                        <li><a href="index_2.html">Home Layer Slider</a></li>
-                        <li><a href="index_6.html">Home Video bg</a></li>
-                        <li><a href="index_3.html">Home Text Rotator</a></li>
-                    </ul>
-                    </li>
-                    <li><a href="room_list.html">Rooms</a></li>
-                    <li class="submenu">
-                    <a href="javascript:void(0);" class="show-submenu">Pages<i class="icon-down-open-mini"></i></a>
-                    <ul>
-                    	<li><a href="../header_2/index.html">Header transparent</a></li>
-                    	<li><a href="room_detail.html">Room detail</a></li>
-                        <li><a href="room_detail_2.html">Room detail date 2</a></li>
-                        <li><a href="about.html">About us</a></li>
-                        <li><a href="faq.html">Faq</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="../coming_soon/index.html">Site launch/Coming soon</a></li>
-                        <li><a href="shortcodes.html">Shortcodes</a></li>
-                        <li><a href="icon_pack_1.html">Icon pack 1</a></li>
-                         <li><a href="icon_pack_2.html">Icon pack 2</a></li>
-                        <li><a href="icon_pack_3.html">Icon pack 3</a></li>
-                    </ul>
-                    </li>
-                    <li><a href="contacts.html">Contacts</a></li>
-                </ul>
-            </div><!-- End main-menu -->
-            
-            </nav>
-        </div><!-- End row -->
-    </div><!-- End container -->
-    </header>
+    <?php include 'includes/nav.php';?>
     <!-- End Header =============================================== -->
     
     <!-- SubHeader =============================================== -->
@@ -86,9 +29,13 @@
     <!-- End SubHeader ============================================ -->
     
     <div class="container margin_60">
-      <h2 class="main_title"><em></em>Welcome to Recom <span>Hotel and Bed&amp;Breakfast</span></h2>
+      <h2 class="main_title"><em></em>Welcome to SuryaVilla <span>Hotel and Bed&amp;Breakfast</span></h2>
         <p class="lead styled">
-            Usu habeo equidem sanctus no. Suas summo id sed, erat erant oporteat cu pri. In eum omnes molestie. Sed ad debet scaevola, ne mel lorem legendos. Unum etiam cum te, an elit assueverit vix, falli aliquam complectitur ex ius.
+            The lush green koregaon park area is so invitingly refreshing, surrounded with an aura of serenity, quietness that is relaxing. Hotel Surya Villa is here to soothe the weary minds. We constantly work towards building a better and sustainable way of living by providing facilities to our customers without compromising on the environment. The homely atmosphere is enhanced by its setting amongst swaying trees in a flourishing garden. Enjoy the tranquility and bird life.
+
+            The well-known Osho Commune is just 5 minutes walking distance away; while M.G. Road, the main shopping area of Poona city, is just 10 minutes drive away.
+
+            Koregaon Park is boomeranging into a software and business-hub of Poona. Hotel Surya Villa located in this vicinity provides the business-people and seekers an ideal place to stay in.
         </p>
 
     	<div class="row about">
